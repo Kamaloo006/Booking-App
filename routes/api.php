303988a4e30a060e8a6357e1af17d308a0f41ae4
Kamal Al Khatib <kamalkhatib1234@gmail.com>
2025-12-17 16:43:32 +0300

    nothing

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::middleware('auth:sanctum')->group(function () {
     // get user by his token
     Route::get('/user/token', [UserController::class, 'getUserFromToken']);
     //get owner's proeperties by his token
-    Route::get('/owner/token',[PropertyController::class,'getPropertiesByOwner']); 
+    Route::get('/owner/token', [PropertyController::class, 'getPropertiesByOwner']);
 
     //--------------------------------------|| Properties
     // create new property
@@ -66,13 +66,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/bookings', [BookingController::class, 'getAllBookings']);
     Route::get('/bookings/cancelled', [BookingController::class, 'getCancelledBookings']);
     Route::get('/bookings/old', [BookingController::class, 'getOldBookings']);
-     Route::get('/bookings/current/future', [BookingController::class, 'getCurrentAndFutureBookings']);
+    Route::get('/bookings/current/future', [BookingController::class, 'getCurrentAndFutureBookings']);
     Route::get('/bookings/current', [BookingController::class, 'getCurrentBookings']);
     Route::get('/bookings/future', [BookingController::class, 'getFutureBookings']);
 
 
-    Route::post('/rating/{booking}',[BookingController::class,'addRating']);
-     Route::put('/updaterating/{booking}',[BookingController::class,'updateRating']);
+    Route::post('/rating/{booking}', [BookingController::class, 'addRating']);
+    Route::put('/updaterating/{booking}', [BookingController::class, 'updateRating']);
 
     // --------------------------------- || Filter Properties
     Route::get('properties', [PropertyController::class, 'filterProperties']);
